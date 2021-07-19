ae6be1093a4e0b5fa5fbfde70a9fb5f9925813c5
SaketSaurab <100rabhsinghalld@gmail.com>
2021-07-19 14:28:38 +0530

    commit

---
+++ app/src/main/java/com/example/covidtracker/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.covidtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
